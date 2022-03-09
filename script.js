c6599a1c6be721c164a9fcc9cd610c11b5d4f1a0
Larissa Vieira <lv23@nyu.edu>
2022-03-09 02:34:17 -0500

    reverted to old way of creating grid using HTML. Figured out how to place first tile with a value of 2.

--- script.js
+++ script.js
@@ -16,12 +16,6 @@ const gameBoard = document.querySelector(".game-board");
 
 const totalTiles = 16;
 
-// function getRandomNum(max) {
-//   let randomNum = Math.floor(Math.random() * max);
-//   console.log(randomNum);
-// }
-// getRandomNum(totalTiles);
-
 // function placeTile() {
 //   for (i = 1; i <= totalTiles; i++) {
 //     // let tileValue = boardTile.value;
@@ -34,12 +28,23 @@ const totalTiles = 16;
 // }
 //console.log(placeTile());
 
+function getRandomNum(max) {
+  let randomNum = Math.floor(Math.random() * max);
+  return randomNum;
+}
+let randomNum = getRandomNum(totalTiles);
+
 function createGrid() {
   for (let i = 1; i <= totalTiles; i++) {
-    let boardTiles = document.createElement("div");
-    boardTiles.classList.add("board-tile");
-    boardTiles.setAttribute("value", 0);
-    gameBoard.appendChild(boardTiles);
+    const boardTile = document.querySelector(".board-tile");
+    let tileValue = boardTile.getAttribute("value");
+    console.log(`this is the tileValue: ${tileValue}`);
+    if (tileValue == 0) {
+      console.log(`This tile should get a value of 2: ${randomNum}`);
+      startTile1 = document.getElementById(`tile-${randomNum}`);
+      startTile1.setAttribute("value", 2);
+      startTile1.innerHTML = "2";
+    }
   }
 }
 createGrid();
