0ad5c87ce507b77f22f183dfed4f8f66e1fbb1da
Larissa Vieira <lv23@nyu.edu>
2022-03-08 22:26:14 -0500

    Added funtion to return random num, will be used later for tile placement. Starting to create Event Listeners for arrow buttons used for movement.

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log("hello world");
+//console.log("hello world");
+//select 2 random tiles and give them a value of "2"
+//const allTiles = document.querySelector(".board-tile");
+
+const upButton = document.querySelector("#up-button");
+// const rightButton = document.querySelector("#right-button");
+// const downButton = document.querySelector("#down-button");
+// const leftButton = document.querySelector("#left-button");
+
+upButton.addEventListener("click", moveTilesUp);
+// rightButton.addEventListener("click", moveTilesRight);
+// downButton.addEventListener("click", moveTilesDown);
+// leftButton.addEventListener("click", moveTilesLeft);
+
+const totalTiles = 16;
+
+function getRandomTile(max) {
+  return Math.floor(Math.random() * max);
+}
+console.log(getRandomTile(totalTiles));
