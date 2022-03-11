a87cb18c8387d5fdd8415031a8dd73a60cff1e9b
Larissa Vieira <lv23@nyu.edu>
2022-03-11 06:01:07 -0500

    Board now starts with 2 random tiles being given a value of 2, but needs reworking

--- script.js
+++ script.js
@@ -1,41 +1,35 @@
-//console.log("hello world");
-//select 2 random tiles and give them a value of "2"
-//const allTiles = document.querySelector(".board-tile");
-
 const gameBoard = document.querySelector(".game-board");
-const newGameButton = document.querySelector("#new-game-button");
 
-const totalTiles = 16;
+const boardTiles = document.querySelectorAll(".board-tile");
+
+const totalTiles = boardTiles.length;
 
 function getRandomNum(max) {
   let randomNum = Math.floor(Math.random() * max);
   return randomNum;
 }
-getRandomNum(totalTiles);
+
+let availTiles = [];
+let tileValue;
 
 function setStartGrid() {
-  const boardTile = document.getElementsByClassName("board-tile");
-  console.log(boardTile);
-  for (let i = 1; i <= totalTiles; i++) {
-    //iterates through all tiles
-    // let tileValue = boardTile.getAttribute("value");
-    //all tiles start with a value of 0
-    //console.log(`this is the tileValue: ${tileValue}`);
-    // console.log(`This tile should get a value of 2: ${randomNum}`);
-    // startTile1 = document.getElementById(`tile-${getRandomNum(totalTiles)}`);
-    // console.log(`The first tile is tile-${randomNum}`);
-    // startTile1.setAttribute("value", 2);
-    // startTile1.innerHTML = "2";
-    //assigns a random tile the value of 2, this is the first tile on the board
-    //now startTile1 must be removed as an option
-    // if (tileValue == 0) {
-    //   console.log(`This tile should get a value of 2: ${randomNum}`);
-    //   startTile2 = document.getElementById(`tile-${randomNum}`);
-    //   startTile2.setAttribute("value", 2);
-    //   startTile2.innerHTML = "2";
-    // }
+  for (let i = 0; i < totalTiles; i++) {
+    tileValue = boardTiles[i].getAttribute("value");
+    tileById = document.getElementById(`${i}`);
+
+    if (tileValue == 0) {
+      availTiles.push(tileById);
+    }
   }
+  startTile1 = document.getElementById(`${getRandomNum(totalTiles)}`);
+  availTiles.splice([startTile1.id], 1);
+  startTile1.setAttribute("value", 2);
+  startTile1.innerHTML = "2";
+
+  startTile2 = document.getElementById(`${getRandomNum(availTiles.length)}`);
+  availTiles.splice([startTile2.id], 1);
+  startTile2.setAttribute("value", 2);
+  startTile2.innerHTML = "2";
+  console.log(availTiles);
 }
 setStartGrid();
-
-newGameButton.addEventListener("click", setStartGrid());
